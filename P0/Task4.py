cd14def67f14379d94c2629242973532e6789551
cos <cosmineugendinu@gmail.com>
2020-02-13 15:08:27 +0200

    Task4.py completed

--- P0/Task4.py
+++ P0/Task4.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Read file into texts and calls.
 It's ok if you don't understand how to read files.
@@ -24,4 +25,41 @@ Print a message:
 <list of numbers>
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
+
+
+
+def find_telemarketers(calls, texts):
+    telemarketers = set()
+    non_telemarketers = set()
+    # callers comprise non_telemarketers and telemarketers numbers
+    callers = set() 
+
+    for call in calls:
+        caller = call[0]
+        receiver = call[1]
+        
+        callers.add(caller)
+        non_telemarketers.add(receiver)
+    
+    for text in texts:
+        sender = text[0]
+        receiver = text[1]
+
+        non_telemarketers.add(sender)
+        non_telemarketers.add(receiver)
+    
+    telemarketers = callers.difference(non_telemarketers)
+
+    return telemarketers
+    
+def print_result():
+    telemarketers_sorted = sorted(find_telemarketers(calls, texts))
+    telemarketers_str = ''
+    for t in telemarketers_sorted:
+        telemarketers_str += str(t) + '\n'
+    print(
+    "These numbers could be telemarketers: \n" +
+    telemarketers_str
+    )
+print_result()
 
